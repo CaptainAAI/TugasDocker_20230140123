f0f8ac445881cd097f17f127fcd7e3bc83a44cbb
Ananda Agung Ismail <anandaagungismail@gmail.com>
2026-04-13 13:46:07 +0700

    add login function on UserController

--- src/main/java/com/tugas/deploy/controller/UserController.java
+++ src/main/java/com/tugas/deploy/controller/UserController.java
@@ -1,4 +1,33 @@
 package com.tugas.deploy.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+    private final String USERNAME = "admin";
+    private final String PASSWORD = "20230140123";
+
+    @GetMapping("/")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(String username, String password, Model model) {
+        if (USERNAME.equals(username) && PASSWORD.equals(password)) {
+            return "home";
+        } else {
+            model.addAttribute("error", "Invalid username or password");
+            return "login";
+        }
+
+    }
+
+    @GetMapping("/home")
+    public String homePage() {
+        return "home";
+    }
 }
